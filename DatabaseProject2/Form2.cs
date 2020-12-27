fe13f83dab814dfec95e092024489b34b356fb05
Philo <philoatefdev@gmail.com>
2020-12-27 20:42:53 +0200

    Add project files.

---
+++ DatabaseProject2/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseProject2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'projectDataSet.Customer' table. You can move, or remove it, as needed.
+            this.customerTableAdapter.Fill(this.projectDataSet.Customer);
+
+        }
+    }
+}
